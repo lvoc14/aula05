ef39e59993efcecf82ca36f537b6ad2d77def519
leonardo victor <leonardovictor5@hotmail.com>
2017-11-08 20:31:59 -0300

    add dados na tabela sala

--- WebAppPB-Lab2-2017-2/Migrations/Configuration.cs
+++ WebAppPB-Lab2-2017-2/Migrations/Configuration.cs
@@ -1,11 +1,13 @@
 namespace WebAppPB_Lab2_2017_2.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using WebAppPB_Lab2_2017_2.Models;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<WebAppPB_Lab2_2017_2.Models.CinemaContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<CinemaContext>
     {
         public Configuration()
         {
@@ -13,20 +15,42 @@ namespace WebAppPB_Lab2_2017_2.Migrations
             ContextKey = "WebAppPB_Lab2_2017_2.Models.CinemaContext";
         }
 
-        protected override void Seed(WebAppPB_Lab2_2017_2.Models.CinemaContext context)
+        protected override void Seed(CinemaContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            if (context.Salas.Count() == 0)
+            {
+                try
+                {
+                    PopularDados(context);
+                    context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    // TODO: fazer log de erro
+                    ex.ToString();
+                }
+            }
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+        private void PopularDados(CinemaContext contexto)
+        {
+            var salas = new List<Sala>
+            {
+                new Sala
+                {
+                    Descricao = "Sala 1",
+                    Capacidade =50,
+                    Numero = 20
+                },
+                new Sala
+                {
+                    Descricao = "Sala 1",
+                    Capacidade =50,
+                    Numero = 20
+                }
+            };
+
+            contexto.Salas.AddRange(salas);
         }
     }
 }
